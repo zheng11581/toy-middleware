df7a9b128109448ade90df88575410c20ff5b42a
zhenghc <zhenghc@gih.cn>
2022-10-11 16:56:31 +0800

    redis example

--- redis/go-redis/main.go
+++ redis/go-redis/main.go
@@ -89,6 +89,92 @@ func V8Example() {
 	// key2 does not exist
 }
 
+func redisExample() {
+	ctx := context.Background()
+	if err := initClient(); err != nil {
+		return
+	}
+
+	err := rdb.Set(ctx, "score", 100, 0).Err()
+	if err != nil {
+		fmt.Printf("set key: score failed, err:%v\n", err)
+		return
+	}
+
+	val, err := rdb.Get(ctx, "score").Result()
+	if err != nil {
+		fmt.Printf("get key: score failed, err:%v\n", err)
+		return
+	}
+	fmt.Println("score", val)
+
+	val2, err := rdb.Get(ctx, "name").Result()
+	if err == redis.Nil {
+		fmt.Println("key: name does not exist")
+	} else if err != nil {
+		fmt.Printf("get key: name failed, err:%v\n", err)
+		return
+	} else {
+		fmt.Println("name", val2)
+	}
+}
+
+func redisExample2() {
+	ctx := context.Background()
+	if err := initClient(); err != nil {
+		return
+	}
+	zsetKey := "language_rank"
+	languages := []*redis.Z{
+		&redis.Z{Score: 90.0, Member: "Golang"},
+		&redis.Z{Score: 98.0, Member: "Java"},
+		&redis.Z{Score: 95.0, Member: "Python"},
+		&redis.Z{Score: 97.0, Member: "JavaScript"},
+		&redis.Z{Score: 99.0, Member: "C/C++"},
+	}
+	// ZADD
+	num, err := rdb.ZAdd(ctx, zsetKey, languages...).Result()
+	if err != nil {
+		fmt.Printf("zadd failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("zadd %d succ.\n", num)
+
+	// 把Golang的分数加10
+	newScore, err := rdb.ZIncrBy(ctx, zsetKey, 10.0, "Golang").Result()
+	if err != nil {
+		fmt.Printf("zincrby failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("Golang's score is %f now.\n", newScore)
+
+	// 取分数最高的3个
+	ret, err := rdb.ZRevRangeWithScores(ctx, zsetKey, 0, 2).Result()
+	if err != nil {
+		fmt.Printf("zrevrange failed, err:%v\n", err)
+		return
+	}
+	for _, z := range ret {
+		fmt.Println(z.Member, z.Score)
+	}
+
+	// 取95~100分的
+	op := &redis.ZRangeBy{
+		Min: "95",
+		Max: "100",
+	}
+	ret, err = rdb.ZRangeByScoreWithScores(ctx, zsetKey, op).Result()
+	if err != nil {
+		fmt.Printf("zrangebyscore failed, err:%v\n", err)
+		return
+	}
+	for _, z := range ret {
+		fmt.Println(z.Member, z.Score)
+	}
+}
+
 func main() {
-	V8Example()
+	// V8Example()
+	// redisExample()
+	redisExample2()
 }
