029a64dea7d63818e23ad109f092cc4fd86f5ab5
zhenghc <zhenghc@gih.cn>
2022-10-11 17:27:11 +0800

    redis example 2

--- redis/go-redis/main.go
+++ redis/go-redis/main.go
@@ -125,20 +125,21 @@ func redisExample2() {
 		return
 	}
 	zsetKey := "language_rank"
-	languages := []*redis.Z{
-		&redis.Z{Score: 90.0, Member: "Golang"},
-		&redis.Z{Score: 98.0, Member: "Java"},
-		&redis.Z{Score: 95.0, Member: "Python"},
-		&redis.Z{Score: 97.0, Member: "JavaScript"},
-		&redis.Z{Score: 99.0, Member: "C/C++"},
-	}
+	// redis.newz
+	languages := []*redis.Z{}
+	languages = append(languages, &redis.Z{Score: 90.0, Member: "Golang"})
+	languages = append(languages, &redis.Z{Score: 98.0, Member: "Java"})
+	languages = append(languages, &redis.Z{Score: 95.0, Member: "Python"})
+	languages = append(languages, &redis.Z{Score: 97.0, Member: "JavaScript"})
+	languages = append(languages, &redis.Z{Score: 99.0, Member: "C/C++"})
+
 	// ZADD
 	num, err := rdb.ZAdd(ctx, zsetKey, languages...).Result()
 	if err != nil {
 		fmt.Printf("zadd failed, err:%v\n", err)
 		return
 	}
-	fmt.Printf("zadd %d succ.\n", num)
+	fmt.Printf("zadd %d successful.\n", num)
 
 	// 把Golang的分数加10
 	newScore, err := rdb.ZIncrBy(ctx, zsetKey, 10.0, "Golang").Result()
@@ -146,7 +147,7 @@ func redisExample2() {
 		fmt.Printf("zincrby failed, err:%v\n", err)
 		return
 	}
-	fmt.Printf("Golang's score is %f now.\n", newScore)
+	fmt.Printf("Golang's score is increased, now is %f.\n", newScore)
 
 	// 取分数最高的3个
 	ret, err := rdb.ZRevRangeWithScores(ctx, zsetKey, 0, 2).Result()
